b9e4a17814ea7db7f047ccfeb964307e97026514
Md Shoaib Islam <60780232+Shoaib3375@users.noreply.github.com>
2021-03-03 13:34:29 +0600

    Create variable.cs

---
+++ variable.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace FirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "John Abraham"; // String Type
+            Console.WriteLine(name);
+            
+            int myNumber = 017; // Int type
+            myNumber = 01; // declare a variable without assigning the value & Changed the myNumber value
+            Console.WriteLine(myNumber);
+            
+            const float py = 342.34324f; // Declare float type variable. In last of float number 'f'is required 
+            Console.WriteLine(py);
+            
+            // Type of Declare variables
+            // Camel Case: firstName
+            // Pascal Case: FirstName
+            // Hungarian Notation: strFirstName
+            /* For local variables: Camel */
+        }
+    }
+}
